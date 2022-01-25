4e5dd705150a02a318cba23b7bac8ba59c9247fa
SANDEEP-PC\Lekhana Sandeep <lekhanasandeep2018@gmail.com>
2022-01-25 23:15:53 +0530

    Built the API in Express (1st Commit)

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const req = require('express/lib/request');
 
 const app = express(); //create an express application
 
+app.use(express.json());  // Parse json objects 
+
 app.listen(3000, ()=>{
     console.log("Listening is started on Port 3000");
 });
+
+
+let avengerArray = [
+    {id: 1, name: "Captain America"},
+    {id: 2, name: "Thor"},
+    {id: 3, name: "Black Widow"},
+]
 
 //callback function here is called a route handler
 app.get("/", (req, res) =>{
@@ -13,8 +22,9 @@ app.get("/", (req, res) =>{
 });
 
 app.get("/api/avengers", (req, res) =>{
-    let avengers = ['Iron Man','Captain America', 'Thor'];
-    res.send(avengers);
+    //let avengers = ['Iron Man','Captain America', 'Thor'];
+    //res.send(avengers);
+    res.send(avengerArray);
 });
 
 // app.get('/api/avengers/1', (req, res) => {
@@ -24,8 +34,66 @@ app.get("/api/avengers", (req, res) =>{
 
 
 //GET with params
+// app.get('/api/avengers/:avengerId', (req, res) => {
+//     //using 'filterBy' user can filter something typing from url (http://localhost:3000/api/avengers/66000?filterBy=ironman)
+//     let requestParam = req.query.filterBy;
+//     res.send('You have requested for the Avenger Id ' + req.params.avengerId + ' filter By ' + requestParam);
+// });
+
+
+
+// ==       value only
+// ===      value and the data type
 app.get('/api/avengers/:avengerId', (req, res) => {
-    //using 'filterBy' user can filter something typing from url (http://localhost:3000/api/avengers/66000?filterBy=ironman)
-    let requestParam = req.query.filterBy;
-    res.send('You have requested for the Avenger Id ' + req.params.avengerId + ' filter By ' + requestParam);
+   let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
+   if(!avenger){
+       return res.status(404).send("Page not Found (ID not found in system)");
+   }
+   res.status(200).send(avenger);
 });
+
+
+app.post('/api/avengers', (req,res) => {
+
+    // Validation
+    if(!req.body.name){
+        return res.status(400).send("Not all mandotory values are sent");
+    }
+
+    let newAvengerObj = {
+        id: avengerArray.length + 1,
+        name: req.body.name
+    };
+    avengerArray.push(newAvengerObj);
+    res.send(newAvengerObj);
+});
+
+
+app.put('/api/avengers/:avengerId', (req,res) => {
+    let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
+
+    if(!avenger){
+        return res.status(404).send("Page not Found (ID not found in system)");
+    }
+
+    // Validation
+    if(!req.body.name){
+        return res.status(400).send("Not all mandotory values are sent");
+    }
+
+    avenger.name = req.body.name;
+    res.send(avenger);
+});
+
+app.delete('/api/avengers/:avengerId', (req,res) => {
+    let avenger = avengerArray.find(a=>a.id == req.params.avengerId);
+
+    if(!avenger){
+        return res.status(404).send("Page not Found (ID not found in system)");
+    }
+
+    let indexOfAvenger = avengerArray.indexOf(avenger);
+    avengerArray.splice(indexOfAvenger, 1);
+
+    res.send(avenger);
+})
